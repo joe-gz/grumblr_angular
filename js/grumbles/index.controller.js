7a38b981e3bc395c3bc86e822c8e7ff72a120b1a
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2015-12-15 14:58:47 -0500

    adds initial commit

---
+++ js/grumbles/index.controller.js
@@ -0,0 +1,18 @@
+"use strict";
+
+(function (){
+  angular.module("grumbles")
+  .controller("GrumbleIndexController", [
+    GrumbleIndexControllerFunction
+  ])
+  function GrumbleIndexControllerFunction (){
+    this.grumbles = [
+      {
+        title: "I am Grumble One"
+      },
+      {
+        title: "I'm another Grumble"
+      }
+    ];
+  }
+}());
